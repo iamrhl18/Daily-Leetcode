fa3761649e778a6ea7fc2aa9af18f5e9330224d0
Rahul kumar <rahulkrg838286@gmail.com>
2026-08-05 11:42:04 +0530

    Time: 8 ms (7.27%) | Memory: 46 MB (53.63%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,7 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+        String r = new StringBuilder(s).reverse().toString();
+        return s.equals(r);     
+    }
+}
